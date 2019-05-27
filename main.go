49d869a4e3153adf4e3a9b20f772f75afb3f9ca9
nobarudo <esukab@gmail.com>
2019-05-28 02:14:59 +0900

    add turai

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"github.com/gin-gonic/gin"
+	"log"
+	"math/rand"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -11,9 +15,32 @@ func main() {
 	router.LoadHTMLGlob("views/*")
 
 	router.GET("/", indexGET)
+	router.GET("/turai", turaiGET)
 	router.Run(":8080")
 }
 
 func indexGET(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html", gin.H{})
+}
+
+func turaiGET(c *gin.Context) {
+	turaiText := "つらい"
+	lines := []string{}
+	f, err := os.Open("./turai.txt")
+	if err != nil {
+		log.Println(err)
+	}
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	length := len(lines)
+	num := rand.Intn(length)
+
+	log.Println(lines[num])
+	turaiText = lines[num]
+
+	c.String(http.StatusOK, turaiText)
 }
