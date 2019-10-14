0c3e1e81380cac15a9f9792f2515768da075e272
nobarudo <esukab@gmail.com>
2019-10-15 01:43:25 +0900

    modify log setting

--- main.go
+++ main.go
@@ -25,7 +25,8 @@ func main() {
 	router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
 
 		// your custom format
-		return fmt.Sprintf("[%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
 			param.TimeStamp.Format(time.RFC1123),
 			param.Method,
 			param.Path,
